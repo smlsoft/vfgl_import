3573deb3903c030b5545d2b504eddee2958e883b
suteetoe <sutee.toe@gmail.com>
2023-06-20 14:18:12 +0700

    feat:: Add Menu Admin Shop, Admin User

--- src/api/menu.js
+++ src/api/menu.js
@@ -1,5 +1,19 @@
 export const menus = [
   {
+    title: "Shop List",
+    icon: "pi pi-file",
+    to: "/shopadmin/shoplist",
+    name: "adminshoplist",
+    children: [],
+  },
+  {
+    title: "Account List",
+    icon: "pi pi-file",
+    to: "/accountadmin/userlist",
+    name: "adminuserlist",
+    children: [],
+  },
+  {
     title: "นำเข้าข้อมูลรายวัน",
     icon: "pi pi-file",
     to: "/daily/import",
